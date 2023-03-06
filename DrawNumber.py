f5170b2812200efceef5b2691c9742d9b76332c6
HoldinMcGroins <t.junliang08@gmail.com>
2023-03-06 15:47:52 +0800

    first commit

---
+++ DrawNumber.py
@@ -0,0 +1,40 @@
+import tensorflow as tf
+from PIL import Image, ImageOps
+import numpy as np
+import matplotlib.pyplot as plt
+from keras.datasets import mnist
+
+
+model=tf.keras.models.load_model('boston_housing/boston_housing model')
+(x_train,y_train),(x_test,y_test)=mnist.load_data()
+
+image=Image.open() #'picture'
+image_gray = ImageOps.grayscale(image)
+image_small = image_gray.resize((28,28))
+
+# n = np.random.randint(len(x_train))
+# img = x_train[n]
+# print(y_train[n])
+# x_trial = x_train[y_train==2]
+# y_trial = y_train[y_train ==2]
+
+#img = x_trial[0]
+
+img=np.asarray(image_small)
+plt.imshow(img)
+plt.show()
+#print(img.shape)
+
+tf.keras.utils.normalize(img)
+img=img.reshape(1,28,28,1)
+prediction = model.predict(img)[0]
+prediction_list = prediction.tolist()
+max_value = max(prediction_list)
+max_index = prediction_list.index(max_value)
+print("The Predicted number is {}".format(max_index))
+
+#print(y_trial)
+#print(model.weights)
+
+
+
---
+++ boston_housing.py
@@ -0,0 +1,27 @@
+import tensorflow as tf
+from keras.datasets import mnist
+import numpy as np
+
+
+(x_train,y_train),(x_test,y_test)=mnist.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
+x_train = x_train[...,np.newaxis]
+x_test = x_test[..., np.newaxis]
+
+model=tf.keras.models.Sequential([
+    tf.keras.layers.Conv2D(8,(3,3),activation ='relu',input_shape = x_train[0].shape),
+    tf.keras.layers.MaxPool2D((2,2)),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dropout(0.3),
+    # tf.keras.layers.Dense(128,activation='relu',kernel_regularizer='l2'),
+    # tf.keras.layers.Dense(64,activation='relu',kernel_regularizer='l2'),
+    tf.keras.layers.Dense(10,activation='softmax')
+])
+print (x_train[0].shape)
+model.compile(loss='sparse_categorical_crossentropy',metrics=['accuracy','mae'],optimizer='adam')
+model.fit(x_train,y_train,epochs=10, validation_split= 0.2)
+eval =model.evaluate(x_train,y_train)
+print(eval)
+model.save('boston_housing/boston_housing model')
+# model.predict(x_test[10])
+print(model.weights)
